ead6064e75a7ab2f8ddb31183b07494b15bc712c
Robell <robellasfaw@gmail.com>
2025-03-13 12:06:36 +0000

    Add Get enpoint for artists by id

--- routes/artistRoutes.js
+++ routes/artistRoutes.js
@@ -2,9 +2,36 @@ import express from "express"
 import { readArtists } from "../utils/helpers.js"
 const router = express.Router()
 
-router.get("/", (_req, res) => {
-    const artists = readArtists()
-    res.status(200).json(artists)
-})
+router
+    .route("/")
+    .get((_req, res) => {
+        try {
+            const artists = readArtists()
+            res.json(artists)
+        } catch (error) {
+            return readArtists.status(404).send("Bad URL")
+        }
+
+    })
+
+router
+    .route("/:id")
+    .get((req, res) => {
+        try {
+            const artists = readArtists()
+            const selectedArtist = artists.find(
+                (artists) => artists.id === req.params.id
+            );
+
+            if (!selectedArtist) {
+                return res.status(404).json({ message: `Could not find artist with id ${req.params.id}` })
+            }
+
+            res.json(selectedArtist)
+        } catch (error) {
+            return res.status(500).json(error)
+        }
+
+    })
 
 export default router
